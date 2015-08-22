675e45119e075463c72726e9c274b787cc4691e9
Tad Hardesty <tad@platymuus.com>
2015-08-22 18:41:41 -0500

    Change load_buffer to take &[u8]

--- src/wrapper/state.rs
+++ src/wrapper/state.rs
@@ -1334,10 +1334,9 @@ impl State {
   // omitted: luaL_opt (undocumented function)
 
   /// Maps to `luaL_loadbuffer`.
-  pub fn load_buffer(&mut self, buff: &str, sz: size_t, name: &str) -> ThreadStatus {
-    let buff_c_str = CString::new(buff).unwrap();
+  pub fn load_buffer(&mut self, buff: &[u8], name: &str) -> ThreadStatus {
     let name_c_str = CString::new(name).unwrap();
-    let result = unsafe { ffi::luaL_loadbuffer(self.L, buff_c_str.as_ptr(), sz, name_c_str.as_ptr()) };
+    let result = unsafe { ffi::luaL_loadbuffer(self.L, buff.as_ptr() as *const _, buff.len() as u32, name_c_str.as_ptr()) };
     ThreadStatus::from_c_int(result).unwrap()
   }
 
